e8083cdf25eca2ee8f5fafb967be449293d6bb1d
parkjiwon <ji11won@naver.com>
2023-12-24 23:55:13 +0900

    Refactor : 화면 통합 및 변경
    - GroupAllFragment, GroupFavoriteFragment : 화면 통합 및 그에 따른 변경사항
    - GroupAdapter : inner class 수정 및 변경

--- app/src/main/java/com/kuit/conet/UI/Group/GroupAllFragment.kt
+++ app/src/main/java/com/kuit/conet/UI/Group/GroupAllFragment.kt
@@ -1,7 +1,5 @@
 package com.kuit.conet.UI.Group
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,22 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import com.kuit.conet.UI.Plan.DetailFixActivity
-import com.kuit.conet.Data.GroupList.groupList
 import com.kuit.conet.Network.ResponseGetGroup
+import com.kuit.conet.Network.ResultGetGroup
 import com.kuit.conet.Network.RetrofitClient
-import com.kuit.conet.UI.Plan.DetailPastActivity
-import com.kuit.conet.Utils.TAG
-import com.kuit.conet.databinding.FragmentGroupCatetoryAllBinding
+import com.kuit.conet.Utils.LIFECYCLE
+import com.kuit.conet.Utils.NETWORK
+import com.kuit.conet.databinding.FragmentGroupListBinding
 import com.kuit.conet.getAccessToken
 import retrofit2.Call
 import retrofit2.Response
 
-class GroupAllFragment: Fragment() {
+class GroupAllFragment : Fragment() {
 
-    private lateinit var binding: FragmentGroupCatetoryAllBinding
-
-    private lateinit var mContext:Context
+    private var _binding: FragmentGroupListBinding? = null
+    private val binding: FragmentGroupListBinding
+        get() = requireNotNull(_binding) { "GroupAllFragment's binding is null" }
+    private lateinit var groupList: List<ResultGetGroup>
 
 
     override fun onCreateView(
@@ -32,51 +30,52 @@ class GroupAllFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentGroupCatetoryAllBinding.inflate(layoutInflater, container, false)
+        Log.d(LIFECYCLE, "GroupAllFragment - onCreateView() called")
+        _binding = FragmentGroupListBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(LIFECYCLE, "GroupAllFragment - onViewCreated() called")
+    }
+
+    // onResume에서 통신을 하는 이유 : ViewPager의 화면 전환은 onResume 라이프 사이클을 보냄
+    // TODO onResume에서 매번 네트워크 통신을 통해 groupList를 가져오는 것이 맞는 것일까?
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "GroupAllFragment - onResume() 실행")
-//        TODO 서버에서 해당 내용 받아오기
-        RetrofitClient.instance.getGroup("Bearer" + getAccessToken(requireContext())).enqueue(object : retrofit2.Callback<ResponseGetGroup>{
-            override fun onResponse(
-                call: Call<ResponseGetGroup>,
-                response: Response<ResponseGetGroup>
-            ) {
-                if(response.isSuccessful) {
-                    Log.d(TAG, "GroupFragment - getTeam()실행 결과 - 성공")
-                    Log.d(TAG, "response : $response")
-                    Log.d(TAG, "response.body : ${response.body()}")
+        Log.d(LIFECYCLE, "GroupAllFragment - onResume() 실행")
 
-                    groupList = response.body()!!.result
-//                    val groupAdapter = GroupAdapter(requireContext(), groupList)
-                    val groupAdapter = GroupAdapter(mContext, groupList)
+        RetrofitClient.instance.getGroup("Bearer " + getAccessToken(requireContext()))
+            .enqueue(object : retrofit2.Callback<ResponseGetGroup> {
+                override fun onResponse(
+                    call: Call<ResponseGetGroup>,
+                    response: Response<ResponseGetGroup>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.d(
+                            NETWORK, "GroupAllFragment - getTeam()실행 결과 - 성공\n" +
+                                    "response : $response\n" +
+                                    "response.body : ${response.body()}"
+                        )
 
-                    GroupFragment.binding.tvGroupCount.text = groupList.count().toString()
+                        groupList = response.body()!!.result
+                        GroupFragment.binding.tvGroupCount.text = groupList.count().toString()
+                        binding.rvGroupList.adapter = GroupAdapter(requireContext(), groupList)
+                        binding.rvGroupList.layoutManager = GridLayoutManager(context, 2)
 
-                    binding.groupListRv.adapter = groupAdapter
-                    binding.groupListRv.layoutManager = GridLayoutManager(context, 2)
-                } else {
-                    Log.d(TAG, "GroupFragment - getTeam()실행 결과 - 안좋음")
-                    Log.d(TAG, "response - ${response}")
+                    } else {
+                        Log.d(
+                            NETWORK, "GroupAllFragment - getTeam()실행 결과 - 안좋음\n" +
+                                    "response - ${response}"
+                        )
+                    }
                 }
-            }
-            override fun onFailure(call: Call<ResponseGetGroup>, t: Throwable) {
-                Log.d(TAG, "GroupFragment - getTeam()실행 결과 - 실패")
-            }
-        })
-    }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "GroupAllFragment - onStart() 실행")
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        mContext = context
+                override fun onFailure(call: Call<ResponseGetGroup>, t: Throwable) {
+                    Log.d(NETWORK, "GroupAllFragment - getTeam()실행 결과 - 실패")
+                }
+            })
     }
 
 }
--- app/src/main/java/com/kuit/conet/UI/Group/GroupFavoriteFragment.kt
+++ app/src/main/java/com/kuit/conet/UI/Group/GroupFavoriteFragment.kt
@@ -1,6 +1,5 @@
 package com.kuit.conet.UI.Group
 
-import com.kuit.conet.databinding.FragmentGroupCategoryFavoriteBinding
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,64 +7,76 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import com.kuit.conet.Data.GroupList
-import com.kuit.conet.Data.GroupList.favoriteGroupList
 import com.kuit.conet.Network.ResponseGetGroup
+import com.kuit.conet.Network.ResultGetGroup
 import com.kuit.conet.Network.RetrofitClient
-import com.kuit.conet.Utils.TAG
+import com.kuit.conet.Utils.LIFECYCLE
+import com.kuit.conet.Utils.NETWORK
+import com.kuit.conet.databinding.FragmentGroupListBinding
 import com.kuit.conet.getAccessToken
 import retrofit2.Call
 import retrofit2.Response
 
-class GroupFavoriteFragment: Fragment() {
+class GroupFavoriteFragment : Fragment() {
 
-    private lateinit var binding: FragmentGroupCategoryFavoriteBinding
+    private var _binding: FragmentGroupListBinding? = null
+    private val binding: FragmentGroupListBinding
+        get() = requireNotNull(_binding) { "GroupFavoriteFragment's binding is null" }
+    private lateinit var favoriteGroupList: List<ResultGetGroup>
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentGroupCategoryFavoriteBinding.inflate(layoutInflater, container, false)
+        Log.d(LIFECYCLE, "GroupFavoriteFragment - onCreateView() called")
+        _binding = FragmentGroupListBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        Log.d(LIFECYCLE, "GroupFavoriteFragment - onViewCreated() called")
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d(LIFECYCLE, "GroupFavoriteFragment - onStart() called")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "GroupFavoriteFragment - getTeam()실행 결과 - 안좋음")
-        RetrofitClient.instance.getBookmarkGroup("Bearer" + getAccessToken(requireContext())).enqueue(object : retrofit2.Callback<ResponseGetGroup>{
-            override fun onResponse(
-                call: Call<ResponseGetGroup>,
-                response: Response<ResponseGetGroup>
-            ) {
-                if(response.isSuccessful) {
-                    Log.d(TAG, "GroupFavoriteFragment - Retrofit getTeam()실행 결과 - 성공")
-                    Log.d(TAG, "response : $response")
-                    Log.d(TAG, "response.body : ${response.body()}")
+        Log.d(LIFECYCLE, "GroupFavoriteFragment - onResume() called")
 
-                    favoriteGroupList = response.body()!!.result
+        RetrofitClient.instance.getBookmarkGroup("Bearer" + getAccessToken(requireContext()))
+            .enqueue(object : retrofit2.Callback<ResponseGetGroup> {
+                override fun onResponse(
+                    call: Call<ResponseGetGroup>,
+                    response: Response<ResponseGetGroup>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.d(
+                            NETWORK, "GroupFavoriteFragment - Retrofit getTeam()실행 결과 - 성공\n" +
+                                    "response : $response\n" +
+                                    "response.body : ${response.body()}"
+                        )
 
-                    GroupFragment.binding.tvGroupCount.text = favoriteGroupList.count().toString()
+                        favoriteGroupList = response.body()!!.result
 
-                    val groupAdapter = GroupAdapter(requireContext(), favoriteGroupList)
-                    binding.groupListRv.adapter = groupAdapter
-                    binding.groupListRv.layoutManager = GridLayoutManager(context, 2)
-                } else {
-                    Log.d(TAG, "GroupFavoriteFragment - Retrofit getTeam()실행 결과 - 안좋음")
+                        GroupFragment.binding.tvGroupCount.text =
+                            favoriteGroupList.count().toString()
+
+                        val groupAdapter = GroupAdapter(requireContext(), favoriteGroupList)
+                        binding.rvGroupList.adapter = groupAdapter
+                        binding.rvGroupList.layoutManager = GridLayoutManager(context, 2)
+                    } else {
+                        Log.d(NETWORK, "GroupFavoriteFragment - Retrofit getTeam()실행 결과 - 안좋음")
+                    }
                 }
-            }
-            override fun onFailure(call: Call<ResponseGetGroup>, t: Throwable) {
-                Log.d(TAG, "GroupFavoriteFragment - Retrofit getTeam()실행 결과 - 실패")
-            }
-        })
+
+                override fun onFailure(call: Call<ResponseGetGroup>, t: Throwable) {
+                    Log.d(NETWORK, "GroupFavoriteFragment - Retrofit getTeam()실행 결과 - 실패")
+                }
+            })
     }
 }
