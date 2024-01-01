d0a9b222c272676296c07c860a6217aa16b28e22
parkjiwon <ji11won@naver.com>
2024-01-02 03:56:40 +0900

    Refactor : GroupPlusActivity
    - 화면 변경
    - 오류 수정

--- app/src/main/java/com/kuit/conet/UI/Group/GroupPlusActivity.kt
+++ app/src/main/java/com/kuit/conet/UI/Group/GroupPlusActivity.kt
@@ -43,13 +43,17 @@ import java.io.FileOutputStream
 import java.net.URL
 
 class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
-    
+
     private var _binding: ActivityGroupPlusBinding? = null
     private val binding: ActivityGroupPlusBinding
         get() = requireNotNull(_binding) { "GroupPlusActivity's binding is null" }
     lateinit var body: MultipartBody.Part
-    private lateinit var imageFile: File
-    private val option: Int by lazy { intent.getIntExtra("option", 0) }     // 0: 그룹 추가하기 , 1: 그룹 수정하기
+    private val option: Int by lazy {
+        intent.getIntExtra(
+            "option",
+            0
+        )
+    }     // 0: 그룹 추가하기 , 1: 그룹 수정하기
     private val groupId: Int by lazy { intent.getIntExtra("groupId", 0) }
 
     private val activityResult: ActivityResultLauncher<Intent> =
@@ -60,14 +64,13 @@ class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
                 Glide.with(this)
                     .load(uri)
                     .centerCrop()
-                    .into(binding.groupPictureIv)
+                    .into(binding.ivGroupPicture)
 
-                binding.groupPictureIv.setBackgroundResource(R.drawable.border_line)
-                binding.loadingPictureLl.visibility = View.INVISIBLE
-                binding.inputPictureLl.visibility = View.VISIBLE
+                binding.ivGroupPicture.setBackgroundResource(R.drawable.border_line)
+                binding.llGroupPlusLoadingPicture.visibility = View.INVISIBLE
+                binding.llGroupPlusInputPicture.visibility = View.VISIBLE
                 binding.imagePlusBtn.text = "수정"
 
-                imageFile = File(getRealFilePath(uri))
                 //creating a file
                 val file = File(getRealFilePath(uri))
                 val requestBody: RequestBody = file.asRequestBody("image/png".toMediaTypeOrNull())
@@ -92,44 +95,44 @@ class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
         selectOption()
 
 //        clickListener 설정
-        binding.closeIv.setOnClickListener(this)
+        binding.ivClose.setOnClickListener(this)
         binding.imagePlusBtn.setOnClickListener(this)
-        binding.finishTv.setOnClickListener(this)
+        binding.tvFinish.setOnClickListener(this)
 
-        binding.groupNameTf.addTextChangedListener(object : TextWatcher {
+        binding.tfGroupName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
             override fun afterTextChanged(s: Editable?) {
-                if (binding.groupNameTf.text!!.isNotEmpty()) {
-                    binding.groupNameHintTv.visibility = View.GONE
+                if (binding.tfGroupName.text!!.isNotEmpty()) {
+                    binding.tvGroupNameHint.visibility = View.GONE
                 } else {
-                    binding.groupNameHintTv.visibility = View.VISIBLE
+                    binding.tvGroupNameHint.visibility = View.VISIBLE
                 }
 
-                if (binding.groupNameTf.text!!.count() > 20) {
-                    binding.groupNameTil.error = " "
-                    binding.finishTv.isEnabled = false
-                    binding.finishTv.setTextColor(getColor(R.color.gray200)) //R.color.select_enabled 이걸로 했는데 제대로 적용이 안됨
-                } else if (binding.groupNameTf.text!!.isEmpty()) {
-                    binding.finishTv.isEnabled = false
-                    binding.finishTv.setTextColor(getColor(R.color.gray200))
+                if (binding.tfGroupName.text!!.count() > 20) {
+                    binding.tilGroupName.error = " "
+                    binding.tvFinish.isEnabled = false
+                    binding.tvFinish.setTextColor(getColor(R.color.gray200)) //R.color.select_enabled 이걸로 했는데 제대로 적용이 안됨
+                } else if (binding.tfGroupName.text!!.isEmpty()) {
+                    binding.tvFinish.isEnabled = false
+                    binding.tvFinish.setTextColor(getColor(R.color.gray200))
                 } else {
-                    binding.groupNameTil.error = null
-                    binding.finishTv.isEnabled = true
-                    binding.finishTv.setTextColor(getColor(R.color.purpleMain))
+                    binding.tilGroupName.error = null
+                    binding.tvFinish.isEnabled = true
+                    binding.tvFinish.setTextColor(getColor(R.color.purpleMain))
                 }
             }
         })
 
-        binding.groupNameTf.setOnFocusChangeListener { _, hasFocus ->
-            if (binding.groupNameTf.error == null) {
-                binding.groupNameTv.isSelected = !hasFocus
-                binding.groupNameTil.isSelected = !hasFocus
-                if (binding.groupNameTf.text!!.isEmpty()) {
-                    binding.groupNameHintTv.visibility = View.VISIBLE
+        binding.tfGroupName.setOnFocusChangeListener { _, hasFocus ->
+            if (binding.tfGroupName.error == null) {
+                binding.tfGroupName.isSelected = !hasFocus
+                binding.tilGroupName.isSelected = !hasFocus
+                if (binding.tfGroupName.text!!.isEmpty()) {
+                    binding.tvGroupNameHint.visibility = View.VISIBLE
                 } else {
-                    binding.groupNameHintTv.visibility = View.GONE
+                    binding.tvGroupNameHint.visibility = View.GONE
                 }
             }
         }
@@ -137,19 +140,19 @@ class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.close_iv -> finish()
-            
+            R.id.iv_close -> finish()
+
             R.id.finish_tv -> {
-                if (binding.groupNameTf.text != null && binding.groupNameTil.error == null /**/) {
+                if (binding.tfGroupName.text != null && binding.tilGroupName.error == null /**/) {
 //                    TODO : 이름과 사진에 대한 정보를 서버에 보내기, 사진을 파일화 하기
                     when (option) {
                         1 -> {
                             val jsonString =
-                                "{\"teamId\" : ${groupId}, \"teamName\" : \"${binding.groupNameTf.text}\"}"
+                                "{\"teamId\" : ${groupId}, \"teamName\" : \"${binding.tfGroupName.text}\"}"
                             val jsonList =
                                 jsonString.toRequestBody("application/json".toMediaTypeOrNull())
 
-                            if (::body.isInitialized == false) {
+                            if (!::body.isInitialized) {
                                 finish()
                             } else {
                                 RetrofitClient.instance.updateGroup(
@@ -170,21 +173,27 @@ class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
                                             )
                                             finish()
 
-                                        } else Log.d(NETWORK, "GroupPlusActivity - Retrofit updateGroup() 실행결과 - 안좋음")
+                                        } else Log.d(
+                                            NETWORK,
+                                            "GroupPlusActivity - Retrofit updateGroup() 실행결과 - 안좋음"
+                                        )
                                     }
 
                                     override fun onFailure(
                                         call: Call<ResponseUpdateGroup>,
                                         t: Throwable
                                     ) {
-                                        Log.d(NETWORK, "GroupPlusActivity - Retrofit updateGroup() 실행결과 - 실패\n" + "t : $t")
+                                        Log.d(
+                                            NETWORK,
+                                            "GroupPlusActivity - Retrofit updateGroup() 실행결과 - 실패\nt : $t"
+                                        )
                                     }
                                 })
                             }
                         }
 
                         else -> {
-                            val jsonString = "{\"teamName\" : \"${binding.groupNameTf.text}\"}"
+                            val jsonString = "{\"teamName\" : \"${binding.tfGroupName.text}\"}"
                             val jsonList =
                                 jsonString.toRequestBody("application/json".toMediaTypeOrNull())
 
@@ -209,7 +218,7 @@ class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
                                         Log.d(
                                             NETWORK,
                                             "GroupPlusActivity - Retrofit creatTeam() 실행결과 - 성공\n" +
-                                                    "response: ${response}"
+                                                    "response: $response"
                                         )
                                     }
                                 }
@@ -230,9 +239,9 @@ class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.image_plus_btn -> {
-                binding.groupPictureIv.setBackgroundResource(R.drawable.border_dotted_line)
-                binding.loadingPictureLl.visibility = View.VISIBLE
-                binding.inputPictureLl.visibility = View.INVISIBLE
+                binding.ivGroupPicture.setBackgroundResource(R.drawable.border_dotted_line)
+                binding.llGroupPlusLoadingPicture.visibility = View.VISIBLE
+                binding.llGroupPlusInputPicture.visibility = View.INVISIBLE
 
                 // 현재 기기에 설정된 읽기/쓰기 권한을 가져오기 위한 변수
                 val readPermission = ContextCompat.checkSelfPermission(
@@ -250,11 +259,12 @@ class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
                         arrayOf(
                             android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
                             android.Manifest.permission.READ_EXTERNAL_STORAGE
-                        ), 1)
+                        ), 1
+                    )
 
-                    binding.groupPictureIv.setBackgroundResource(R.drawable.border_line)
-                    binding.loadingPictureLl.visibility = View.INVISIBLE
-                    binding.inputPictureLl.visibility = View.VISIBLE
+                    binding.ivGroupPicture.setBackgroundResource(R.drawable.border_line)
+                    binding.llGroupPlusLoadingPicture.visibility = View.INVISIBLE
+                    binding.llGroupPlusInputPicture.visibility = View.VISIBLE
                     binding.imagePlusBtn.text = "첨부"
 
                 } else {// 위 경우가 아니라면 권한에 대해서 설정이 되어 있으므로
@@ -272,48 +282,31 @@ class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        if (ev?.action === MotionEvent.ACTION_DOWN) {
-            val v = currentFocus
-            if (v is TextInputEditText) {
-                val outRect = Rect()
-                v.getGlobalVisibleRect(outRect)
-                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
-                    v.clearFocus()
-                    val imm: InputMethodManager =
-                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
-                }
-            }
-        }
-        return super.dispatchTouchEvent(ev)
-    }
-
     private fun selectOption() {
         when (option) {
-            1 -> {//모임이 생성된 상태에서 모임 정보 수정시
-                binding.finishTv.setTextColor(getColor(R.color.purpleMain))
-                binding.titleTv.text = "모임 수정하기"
+            1 -> {  // 모임 수정시
+                binding.tvFinish.setTextColor(getColor(R.color.purpleMain))
+                binding.tvTitle.text = "모임 수정하기"
 //                TODO 이전화면에 있는 데이터 가져오기
-                binding.groupNameTf.setText(intent.getStringExtra("groupName"))
-                binding.groupNameHintTv.text = ""
+                binding.tfGroupName.setText(intent.getStringExtra("groupName"))
+                binding.tvGroupNameHint.text = ""
                 val image = intent.getStringExtra("groupImage")
                 val imageUri = Uri.parse(image)
                 Glide.with(this)
                     .load(imageUri)
                     .centerCrop()
-                    .into(binding.groupPictureIv)
+                    .into(binding.ivGroupPicture)
                 binding.imagePlusBtn.text = "수정"
-                binding.finishTv.isEnabled = true
+                binding.tvFinish.isEnabled = true
             }
 
-            else -> {//모임을 생성하는 단계
-                binding.titleTv.text = "모임 추가하기"
+            else -> {   // 모임 생성시
+                binding.tvTitle.text = "모임 추가하기"
             }
         }
     }
 
-    private fun changeUrltoFile(groupImgUrl: String) {
+    /*private fun changeUrltoFile(groupImgUrl: String) {
         val url = URL(groupImgUrl)
         val connection = url.openConnection()
         connection.doInput = true
@@ -335,7 +328,7 @@ class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
 
         val requestBody: RequestBody = tempFile.asRequestBody("image/png".toMediaType())
         body = MultipartBody.Part.createFormData("file", tempFile.name, requestBody)
-    }
+    }*/
 
     //    uri를 가지고 해당 파일의 진짜 위치를 보낸다.
     private fun getRealFilePath(uri: Uri): String {
@@ -347,13 +340,31 @@ class GroupPlusActivity : AppCompatActivity(), View.OnClickListener {
         var columnIndex = 0
         val proj = arrayOf(MediaStore.Images.Media.DATA)
         val cursor = contentResolver.query(uri, proj, null, null, null)
-
-        if (cursor!!.moveToFirst()) {
-            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        val filePath = cursor.use {// This Cursor should be freed up after use with #close()에 대한 처리문이다.
+            if (cursor!!.moveToFirst()) {
+                columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            }
+            cursor.getString(columnIndex)
         }
 
-        return cursor.getString(columnIndex)
+        return filePath
     }
 
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev?.action === MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is TextInputEditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm: InputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
 
 }
