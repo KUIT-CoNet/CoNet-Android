e169b1d00e214d8424589724319898f3d7a5b0c1
parkjiwon <ji11won@naver.com>
2024-01-15 04:38:51 +0900

    Feat : GroupMainActivity 통신 방법 변경

--- app/src/main/java/com/kuit/conet/UI/GroupMain/GroupMainActivity.kt
+++ app/src/main/java/com/kuit/conet/UI/GroupMain/GroupMainActivity.kt
@@ -1,21 +1,26 @@
 package com.kuit.conet.UI.GroupMain
 
 import android.content.Intent
+import android.icu.util.Calendar
+import android.os.Build
+import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import com.bumptech.glide.Glide
 import com.kuit.conet.Network.ResponseDeleteBookmark
 import com.kuit.conet.Network.ResponseEnrollBookmark
 import com.kuit.conet.Network.RetrofitClient
 import com.kuit.conet.R
+import com.kuit.conet.UI.Group.GroupAdapter
+import com.kuit.conet.UI.Group.GroupData
 import com.kuit.conet.UI.Plan.CreatePlanActivity
+import com.kuit.conet.UI.Plan.detail.PlanListActivity
 import com.kuit.conet.Utils.*
 import com.kuit.conet.databinding.ActivityGroupMainBinding
 import com.kuit.conet.getAccessToken
 import com.prolificinteractive.materialcalendarview.CalendarDay
-import com.prolificinteractive.materialcalendarview.MaterialCalendarView
-import com.prolificinteractive.materialcalendarview.OnDateSelectedListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -26,176 +31,202 @@ import java.util.*
 
 class GroupMainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityGroupMainBinding
-    lateinit var oncallList : OncallList
+    private lateinit var binding: ActivityGroupMainBinding
+    private val fragmentManager = supportFragmentManager
+    private lateinit var groupData: GroupData
 
-
-    override fun onResume() {
-        super.onResume()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityGroupMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val title = intent.getStringExtra("GroupName")
-        val image = intent.getStringExtra("GroupImg")
-        val groupId = intent.getIntExtra("GroupId", 0)
-        val groupMemberCount = intent.getIntExtra("GroupMemberCount", 0)
-        val groupFavoriteTag: Boolean = intent.getBooleanExtra("GroupFavorite", false)
-        binding.tvGroupName.text = title
-        binding.tvCount.text = groupMemberCount.toString() + "명"
-        checkFavoriteTag(groupFavoriteTag)
-        //binding.ivGroupImage.setImageResource(image)
-        Glide.with(this)
-            .load(image) // 불러올 이미지 url
+
+        var _groupData: GroupData?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            _groupData =
+                intent.getSerializableExtra(GroupAdapter.INTENT_GROUP, GroupData::class.java)
+        } else {
+            _groupData = intent.getSerializableExtra(GroupAdapter.INTENT_GROUP) as GroupData?
+        }
+        groupData = requireNotNull(_groupData)
+
+        binding.tvGroupName.text = groupData.groupName
+        binding.tvCount.text = "${groupData.groupMemberCount} 명"
+        checkFavoriteTag(groupData.favoriteTag)
+        Glide.with(this@GroupMainActivity)
+            .load(groupData.groupUrl) // 불러올 이미지 url
             .centerCrop()
             .placeholder(R.drawable.profile_purple) // 이미지 로딩 시작하기 전 표시할 이미지
             .error(R.drawable.profile_purple) // 로딩 에러 발생 시 표시할 이미지
             .fallback(R.drawable.profile_purple) // 로드할 url 이 비어있을(null 등) 경우 표시할 이미지
             .into(binding.ivGroupImage) // 이미지를 넣을 뷰
 
-        val groupCalendarFragment = GroupCalendarFragment(groupId)
-        groupCalendarFragment.setOnDateChangedListener(object : OnDateSelectedListener {
-            override fun onDateSelected(
-                widget: MaterialCalendarView,
-                date: CalendarDay,
-                selected: Boolean
-            ) {
-                Log.d("인터페이스","실행")
-                val selectedDateString = dateString(date.date, 1)
-                val today = Date() // 오늘 날짜
-                val todayString = dateString(today, 1)
-                // 로그로 선택한 날짜 정보 출력
-                Log.d("Selected date", "${selectedDateString}")
-
-                if (todayString == selectedDateString) {
-                    binding.tvPromiseDate.text = "오늘의 약속"
-                } else {
-                    binding.tvPromiseDate.text = selectedDateString + "의 약속"
-                }
-                Log.d("dateString테스트", "${dateString(date.date, 2)}")
-                val todolist = Todolist(date, groupId)
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fl_todolist, todolist)
-                    .commitAllowingStateLoss()
-                CoroutineScope(Dispatchers.Main).launch {
-                    todolist.waitForInit()
-                    binding.tvPromiseCount.text = todolist.returnsize().toString()
-                }
-            }
-        })
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fl_group_calendar, groupCalendarFragment)
-            .commitAllowingStateLoss()
-
-        oncallList = OncallList(groupId, 1)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fl_oncalllist, oncallList)
-            .commitAllowingStateLoss()
-
-        CoroutineScope(Dispatchers.Main).launch {
-            oncallList.waitForInit()
-            binding.tvPromiseallCount.text = oncallList.returnsize().toString()
-        }
-
         binding.ivBackBtn.setOnClickListener {
             finish()
         }
 
-        binding.starIv.setOnClickListener {
-            binding.starIv.visibility = View.GONE
-            binding.starUnIv.visibility = View.VISIBLE
-            RetrofitClient.instance.deleteBookmark("Bearer " + getAccessToken(this),
-                groupId).enqueue(object: retrofit2.Callback<ResponseDeleteBookmark>{
+        binding.ivStar.setOnClickListener {
+            binding.ivStar.visibility = View.GONE
+            binding.ivStarUn.visibility = View.VISIBLE
+            RetrofitClient.instance.deleteBookmark(
+                "Bearer " + getAccessToken(this),
+                groupData.groupId
+            ).enqueue(object : retrofit2.Callback<ResponseDeleteBookmark> {
                 override fun onResponse(
                     call: Call<ResponseDeleteBookmark>,
                     response: Response<ResponseDeleteBookmark>
                 ) {
-                    if(response.isSuccessful){
-                        Log.d(TAG, "GroupMainActivity - Retrofit deleteBookmark() 실행결과 - 성공\n" +
-                                "result : ${response.body()!!.result}")
+                    if (response.isSuccessful) {
+                        Log.d(
+                            NETWORK, "GroupMainActivity - Retrofit deleteBookmark() 실행결과 - 성공\n" +
+                                    "result : ${response.body()?.result}"
+                        )
                     } else {
-                        Log.d(TAG, "GroupMainActivity - Retrofit deleteBookmark() 실행결과 - 안좋음")
+                        Log.d(NETWORK, "GroupMainActivity - Retrofit deleteBookmark() 실행결과 - 안좋음")
                     }
                 }
+
                 override fun onFailure(call: Call<ResponseDeleteBookmark>, t: Throwable) {
-                    Log.d(TAG, "GroupMainActivity - Retrofit deleteBookmark() 실행결과 - 실패")
+                    Log.d(NETWORK, "GroupMainActivity - Retrofit deleteBookmark() 실행결과 - 실패")
                 }
             })
         }
-
-        binding.starUnIv.setOnClickListener {
-            binding.starIv.visibility = View.VISIBLE
-            binding.starUnIv.visibility = View.GONE
-            RetrofitClient.instance.enrollBookmark("Bearer " + getAccessToken(this),
-                groupId).enqueue(object: retrofit2.Callback<ResponseEnrollBookmark>{
+        binding.ivStarUn.setOnClickListener {
+            binding.ivStar.visibility = View.VISIBLE
+            binding.ivStarUn.visibility = View.GONE
+            RetrofitClient.instance.enrollBookmark(
+                "Bearer " + getAccessToken(this),
+                groupData.groupId
+            ).enqueue(object : retrofit2.Callback<ResponseEnrollBookmark> {
                 override fun onResponse(
                     call: Call<ResponseEnrollBookmark>,
                     response: Response<ResponseEnrollBookmark>
                 ) {
-                    if(response.isSuccessful){
-                        Log.d(TAG, "GroupMainActivity - Retrofit enrollBookmark() 실행결과 - 성공\n" +
-                                "result : ${response.body()!!.result}")
+                    if (response.isSuccessful) {
+                        Log.d(
+                            NETWORK, "GroupMainActivity - Retrofit enrollBookmark() 실행결과 - 성공\n" +
+                                    "result : ${response.body()?.result}"
+                        )
                     } else {
-                        Log.d(TAG, "GroupMainActivity - Retrofit enrollBookmark() 실행결과 - 안좋음")
+                        Log.d(NETWORK, "GroupMainActivity - Retrofit enrollBookmark() 실행결과 - 안좋음")
                     }
                 }
 
                 override fun onFailure(call: Call<ResponseEnrollBookmark>, t: Throwable) {
-                    Log.d(TAG, "GroupMainActivity - Retrofit enrollBookmark() 실행결과 - 실패")
+                    Log.d(NETWORK, "GroupMainActivity - Retrofit enrollBookmark() 실행결과 - 실패")
                 }
 
             })
         }
-        val sideBar = SideBar(title, groupMemberCount, groupId, this, image)
+
         binding.ivMenuBtn.setOnClickListener {
-            binding.flSidebar.visibility = View.VISIBLE
-            supportFragmentManager.beginTransaction()
-                .setCustomAnimations(R.anim.to_left, R.anim.from_right)
-                .replace(R.id.fl_sidebar, sideBar)
-                .commitAllowingStateLoss()
+            val sideBar = SideBar(
+                groupData.groupName,
+                groupData.groupMemberCount,
+                groupData.groupId,
+                this,
+                groupData.groupUrl
+            )
+
+            fragmentManager.commit {
+                add(R.id.fl_group_image, sideBar)
+                setCustomAnimations(R.anim.to_left, R.anim.from_right)
+            }
+
+            sideBar.setOnItemClickListener(object : SideBar.OnItemClickListener {
+                override fun onItemClick(option: Int) {
+                    showDetail(option, groupData.groupId)
+                }
+
+                override fun exitSidebar() {
+                    fragmentManager.commit {
+                        val sideBarFragment =
+                            fragmentManager.findFragmentById(R.id.fl_group_image) as SideBar
+                        if (sideBarFragment != null) remove(sideBarFragment)
+                    }
+                }
+            })
         }
 
-        sideBar.setOnItemClickListener(object : SideBar.OnItemClickListener{
-            override fun onItemClick(option: Int) {
-                showDetail(option, groupId)
-            }
-
-            override fun exitSidebar() {
-                binding.flSidebar.visibility = View.GONE
-            }
-        })
-
-        binding.cvMakePlan.setOnClickListener {
+        binding.tvPlanMaker.setOnClickListener {
             val intent = Intent(this, CreatePlanActivity::class.java)
-            intent.putExtra("GroupId",groupId)
+            intent.putExtra(INTENT_GROUP_ID, groupData.groupId)
             startActivity(intent)
         }
 
-    }
-    fun dateString(date : Date, option : Int) : String{ // option이 1이면 m월 d일 형식, option이 2면 yyyy년 m월 d일 형식
-        var dateFormat : SimpleDateFormat
-        if(option == 1){
-            dateFormat = SimpleDateFormat("M월 d일", Locale.getDefault())
+        /*val todolist = Todolist(CalendarDay.today(), groupData.groupId)
+        fragmentManager.commit {
+            replace(R.id.fl_todolist, todolist)
         }
-        else{
-            dateFormat = SimpleDateFormat("yyyy년 M월 d일", Locale.getDefault())
-        }
-        val selectedDateString = dateFormat.format(date)
-        return selectedDateString
+        CoroutineScope(Dispatchers.Main).launch {
+            todolist.waitForInit()
+            binding.tvGroupMainSelectPromiseCount.text = todolist.returnsize().toString()
+        }*/
     }
 
-    fun showDetail(option : Int, groupId : Int){
-        binding.flSidebarMenu.visibility = View.VISIBLE
-        supportFragmentManager.beginTransaction()
+    override fun onResume() {
+        super.onResume()
+        Log.d(LIFECYCLE, "GroupMainActivity - onResume() called")
+
+        val groupCalendarFragment = GroupCalendarFragment(groupData.groupId)
+        groupCalendarFragment.setOnDateChangedListener { widget, date, selected ->
+            val selectedDateString = dateString(date.date, 1)
+            val today = Date() // 오늘 날짜
+            val todayString = dateString(today, 1)
+            if (todayString == selectedDateString) {
+                binding.tvGroupMainSelectDayTitle.text = R.string.today_plan.toString()
+            } else {
+                binding.tvGroupMainSelectDayTitle.text = selectedDateString + "의 약속"
+            }
+
+            val todolist = Todolist(date, groupData.groupId)
+            fragmentManager.commit {
+                replace(R.id.fl_todolist, todolist)
+            }
+            CoroutineScope(Dispatchers.Main).launch {
+                todolist.waitForInit()
+                binding.tvGroupMainSelectPromiseCount.text = todolist.returnsize().toString()
+            }
+        }
+        fragmentManager.commit {
+            replace(R.id.fl_group_calendar, groupCalendarFragment)
+        }
+    }
+
+    private fun dateString(
+        date: Date,
+        option: Int
+    ): String { // option이 1이면 m월 d일 형식, option이 2면 yyyy년 m월 d일 형식
+        val dateFormat: SimpleDateFormat = if (option == 1) {
+            SimpleDateFormat("M월 d일", Locale.getDefault())
+        } else {
+            SimpleDateFormat("yyyy년 M월 d일", Locale.getDefault())
+        }
+        return dateFormat.format(date)
+    }
+
+    fun showDetail(option: Int, groupId: Int) {
+        /*binding.flSidebarMenu.visibility = View.VISIBLE
+        fragmentManager.beginTransaction()
             .replace(R.id.fl_sidebar_menu, SideBarDetailMenu(option, groupId))
-            .commit()
+            .commit()*/
+        val intent = Intent(this, PlanListActivity::class.java)
+        intent.putExtra(INTENT_GROUP_ID, groupId)
+        intent.putExtra(INTENT_SIDE_OPTION, option)
+        startActivity(intent)
     }
 
-    fun checkFavoriteTag(favortieTag: Boolean){
-        if(favortieTag){
-            binding.starIv.visibility = View.VISIBLE
-            binding.starUnIv.visibility = View.GONE
-        } else{
-            binding.starIv.visibility = View.GONE
-            binding.starUnIv.visibility = View.VISIBLE
+    private fun checkFavoriteTag(favoriteTag: Boolean) {
+        if (favoriteTag) {
+            binding.ivStar.visibility = View.VISIBLE
+            binding.ivStarUn.visibility = View.GONE
+        } else {
+            binding.ivStar.visibility = View.GONE
+            binding.ivStarUn.visibility = View.VISIBLE
         }
+    }
+
+    companion object {
+        const val INTENT_GROUP_ID = "GroupId"
+        const val INTENT_SIDE_OPTION = "OPTION"
     }
 }
