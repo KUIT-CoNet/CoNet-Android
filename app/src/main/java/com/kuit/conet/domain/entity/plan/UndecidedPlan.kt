51f5e9df48bef4bdf23846213fa62b3142b5dfb0
parkjiwon <ji11won@naver.com>
2024-03-05 18:46:04 +0900

    Feat : 확정되지 않은 약속 DTO 추가

---
+++ app/src/main/java/com/kuit/conet/domain/entity/plan/UndecidedPlan.kt
@@ -0,0 +1,9 @@
+package com.kuit.conet.domain.entity.plan
+
+data class UndecidedPlan(
+    val planId: Long,
+    val planName: String,
+    val groupName: String,
+    val startDate: String,
+    val endDate: String,
+)
