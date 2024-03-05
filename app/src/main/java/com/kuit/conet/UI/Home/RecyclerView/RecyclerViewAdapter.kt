ef86bc3f559d02bfee812d87ebd8472380e96455
parkjiwon <ji11won@naver.com>
2024-03-05 18:51:40 +0900

    Refactor : 대기중인 약속 item 리펙토링
    - item_todolistall : 대기중인 약속 layout 수정
    - RecyclerViewAdapter/AllTodoRecyclerAdapter : 대기중인 약속 adapter 리펙토링

--- app/src/main/java/com/kuit/conet/UI/Home/RecyclerView/RecyclerViewAdapter.kt
+++ app/src/main/java/com/kuit/conet/UI/Home/RecyclerView/RecyclerViewAdapter.kt
@@ -2,7 +2,6 @@ package com.kuit.conet.UI.Home.RecyclerView
 
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,66 +9,70 @@ import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.kuit.conet.Network.Oncall
 import com.kuit.conet.Network.Plan
-//import com.kuit.conet.Network.SidePlanInfo
 import com.kuit.conet.UI.Plan.detail.DetailFixActivity
-import com.kuit.conet.UI.Plan.delete.DetailPastActivity
 import com.kuit.conet.UI.Plan.PlanTimeActivity
-import com.kuit.conet.Utils.TAG
-import com.kuit.conet.databinding.ItemCofirmlistBinding
 import com.kuit.conet.databinding.ItemTodolistBinding
 import com.kuit.conet.databinding.ItemTodolistallBinding
+import com.kuit.conet.domain.entity.plan.DecidedPlan
+import com.kuit.conet.domain.entity.plan.UndecidedPlan
 
-class AllTodoRecyclerAdapter(option : Int, context: Context) : RecyclerView.Adapter<AllTodoRecyclerAdapter.ViewHolder>(){
-    var datas = ArrayList<Oncall>()
-    val option = option
-    val context = context
+class AllTodoRecyclerAdapter(
+    private val data: List<UndecidedPlan>,
+    private val option: Int,
+    private val context: Context
+) : RecyclerView.Adapter<AllTodoRecyclerAdapter.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemTodolistallBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+        val binding =
+            ItemTodolistallBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding, option, context)
     }
 
-    override fun getItemCount(): Int = datas.size
+    override fun getItemCount(): Int = data.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(datas[position])
-
-        holder.binding.clOncalllist.setOnClickListener {
-            val mIntent = Intent(context, PlanTimeActivity::class.java)
-            mIntent.putExtra("planId",datas[position].planId)
-            mIntent.putExtra("planName",datas[position].planName)
-            startActivity(context, mIntent, null)
-        }
+        holder.bind(data[position])
     }
 
-    inner class ViewHolder(val binding: ItemTodolistallBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(
+        val binding: ItemTodolistallBinding,
+        private val option: Int,
+        private val context: Context
+    ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: Oncall) {
+        fun bind(item: UndecidedPlan) {
             binding.tvPromiseStartdate.text = item.startDate
             binding.tvPromiseEnddate.text = item.endDate
             binding.tvPromisscontent.text = item.planName
-            if(item.teamName == null){
+            binding.tvPromisscategory.text = item.groupName
 
-                binding.tvPromisscategory.visibility = View.GONE
-            }
-            else{
-                binding.tvPromisscategory.text= item.teamName
-            }
             val density: Float = context.resources.displayMetrics.density
-            if(option == 1){ // 메인화면에 띄울때
-                binding.clOncalllist.elevation = 8F*density
-            }
-            else{ // 사이드바 메뉴에 띄울 때
-                binding.clOncalllist.elevation = 0F*density
+            when (option) {
+                1 -> { // 메인화면에 띄울때
+                    binding.clOncalllist.elevation = 8F * density
+                }
+
+                else -> { // 사이드바 메뉴에 띄울 때
+                    binding.clOncalllist.elevation = 0F * density
+                }
             }
 
+            binding.clOncalllist.setOnClickListener {
+                val mIntent = Intent(context, PlanTimeActivity::class.java)
+                mIntent.putExtra("planId", item.planId.toInt())
+                mIntent.putExtra("planName", item.planName)
+                startActivity(context, mIntent, null)
+            }
         }
     }
 }
 
-class TodoRecyclerAdapter(private val context:Context) : RecyclerView.Adapter<TodoRecyclerAdapter.ViewHolder>(){
+class TodoRecyclerAdapter(private val context: Context) :
+    RecyclerView.Adapter<TodoRecyclerAdapter.ViewHolder>() {
     var itemList = ArrayList<Plan>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemTodolistBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemTodolistBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -85,16 +88,16 @@ class TodoRecyclerAdapter(private val context:Context) : RecyclerView.Adapter<To
         }
     }
 
-    inner class ViewHolder(val binding: ItemTodolistBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(val binding: ItemTodolistBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Plan) {
             binding.tvPromisstime.text = item.time
             binding.tvPromisscontent.text = item.planName
-            if(item.teamName == null){
+            if (item.teamName == null) {
                 binding.tvPromisscategory.visibility = View.GONE
-            }
-            else{
-                binding.tvPromisscategory.text= item.teamName
+            } else {
+                binding.tvPromisscategory.text = item.teamName
             }
 
         }
