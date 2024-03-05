81531e78d04f51fa39a0bb022f006a74071f0541
parkjiwon <ji11won@naver.com>
2024-03-05 18:54:22 +0900

    Refactor : 대기중인 약속 Fragment 리펙토링
    - fragment_oncall : 레이아웃 수정
    - OncallList : 리펙토링, 통신부분 새로운 api와 연결완료

--- app/src/main/java/com/kuit/conet/Utils/OncallList.kt
+++ app/src/main/java/com/kuit/conet/Utils/OncallList.kt
@@ -1,19 +1,17 @@
 package com.kuit.conet.Utils
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.kuit.conet.Network.HomeOncall
-import com.kuit.conet.Network.Oncall
-import com.kuit.conet.Network.RetrofitInterface
-import com.kuit.conet.Network.getRetrofit
+import com.kuit.conet.Network.RetrofitClient
 import com.kuit.conet.UI.Home.RecyclerView.AllTodoRecyclerAdapter
+import com.kuit.conet.data.dto.response.home.ResponseGetWaitingPlan
+import com.kuit.conet.data.dto.response.plan.ResponseGetGroupWaitingPlan
 import com.kuit.conet.databinding.FragmentOncallBinding
+import com.kuit.conet.domain.entity.plan.UndecidedPlan
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -21,139 +19,127 @@ import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import kotlin.collections.ArrayList
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
-class OncallList(groupId : Int, option : Int) : Fragment() { // -1이면 홈 화면 용 1이상이면 모임 화면 용(이 값이 groupId역할을 할 거임)
-    lateinit var binding : FragmentOncallBinding
-    lateinit var allTodoRecyclerAdapter: AllTodoRecyclerAdapter
-    lateinit var mContext : Context
-    var oncall = ArrayList<Oncall>()
-    val groupId = groupId
-    val option = option // 사이드바 메뉴에 띄울 때랑 그냥 메인 페이지에 띄울 때랑 그림자 속성이 달라서 option변수 사용
+class OncallList(
+    private val groupId: Int,
+    private val option: Int     // -1 : HomeFragment, 1 : GroupMainActivity
+) : Fragment() {
 
+    private var _binding: FragmentOncallBinding? = null
+    private val binding: FragmentOncallBinding
+        get() = requireNotNull(_binding) { "OncallList's binding is null" }
+
+    private lateinit var allTodoRecyclerAdapter: AllTodoRecyclerAdapter
     private val initDeferred = CompletableDeferred<Unit>()
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        mContext = context
-    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        binding = FragmentOncallBinding.inflate(inflater, container, false)
+        _binding = FragmentOncallBinding.inflate(inflater, container, false)
+        Log.d(LIFECYCLE, "OncallList - onCreateView() called")
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(LIFECYCLE, "OncallList - onViewCreated() called")
     }
 
     override fun onResume() {
         super.onResume()
-        val coroutineScope = CoroutineScope(Dispatchers.Main)
-        coroutineScope.launch {
-            if(groupId < 0){
-                oncall = showOncall()
+        Log.d(LIFECYCLE, "OncallList - onResume() called")
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val oncall = if (groupId < 0) {
+                showOncall()
+            } else {
+                showGroupOncall()
             }
-            else{
-                oncall = showGroupOncall()
-            }
+
             initAllRecycler(oncall)
             initDeferred.complete(Unit)
         }
     }
 
-    suspend fun waitForInit(){
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+        Log.d(LIFECYCLE, "OncallList - onDestroyView() called")
+    }
+
+    private suspend fun showOncall(): List<UndecidedPlan> {
+        return suspendCoroutine { continuation ->
+            RetrofitClient.homeInstance.getWaitingPlan(
+                authorization = "Bearer ${getRefreshToken(requireContext())}",
+            ).enqueue(object : Callback<ResponseGetWaitingPlan> {
+                override fun onResponse(
+                    call: Call<ResponseGetWaitingPlan>,
+                    response: Response<ResponseGetWaitingPlan>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.d(NETWORK, "OncallList - getWaitingPlan() 실행결과 - 성공")
+                        val resp =
+                            requireNotNull(response.body()) { "OncallList - getWaitingPlan() 결과 불러오기 실패" }
+                        continuation.resume(resp.result.plans.map { it.asUndecidedPlan() })
+                    } else {
+                        Log.d(NETWORK, "OncallList - getWaitingPlan() 실행결과 - 안좋음")
+                        continuation.resumeWithException(Exception("Response not successful"))
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseGetWaitingPlan>, t: Throwable) {
+                    Log.d(NETWORK, "OncallList - getWaitingPlan() 실행결과 - 실패\nbecause : $t")
+                    continuation.resumeWithException(t)
+                }
+
+            })
+        }
+
+    }
+
+    private suspend fun showGroupOncall(): List<UndecidedPlan> {
+        return suspendCoroutine { continuation ->
+            RetrofitClient.planInstance.getGroupWaitingPlan(
+                teamId = groupId.toLong()
+            ).enqueue(object : Callback<ResponseGetGroupWaitingPlan> {
+                override fun onResponse(
+                    call: Call<ResponseGetGroupWaitingPlan>,
+                    response: Response<ResponseGetGroupWaitingPlan>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.d(NETWORK, "OncallList - getGroupWaitingPlan() 실행결과 - 성공")
+                        val resp =
+                            requireNotNull(response.body()) { "OncallList - getGroupWaitingPlan() 실행결과 불러오기 실패" }
+                        continuation.resume(resp.result.plans.map { it.asUndecidedPlan() })
+                    } else {
+                        Log.d(NETWORK, "OncallList - getGroupWaitingPlan() 실행결과 - 안좋음")
+                        continuation.resumeWithException(Exception("Response not successful"))
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseGetGroupWaitingPlan>, t: Throwable) {
+                    Log.d(NETWORK, "OncallList - getGroupWaitingPlan() 실행결과 - 실패\nbecause : $t")
+                    continuation.resumeWithException(t)
+                }
+
+            })
+        }
+    }
+
+    private fun initAllRecycler(oncall: List<UndecidedPlan>) {
+        val data = oncall.ifEmpty { listOf() }
+        allTodoRecyclerAdapter = AllTodoRecyclerAdapter(data, option, requireActivity())
+        binding.rvAlltodolist.adapter = allTodoRecyclerAdapter
+    }
+
+    suspend fun waitForInit() {
         initDeferred.await()
     }
 
-    fun initAllRecycler(oncall : ArrayList<Oncall>){
-        if(oncall != null){
-            allTodoRecyclerAdapter = AllTodoRecyclerAdapter(option,mContext)
-            binding.rvAlltodolist.adapter = allTodoRecyclerAdapter
-            binding.rvAlltodolist.layoutManager = LinearLayoutManager(context)
-            allTodoRecyclerAdapter.datas = oncall
-            allTodoRecyclerAdapter.notifyDataSetChanged()
-//            binding.tvPromiseallCount.text = date.count().toString()
-        }
-        else{
-            allTodoRecyclerAdapter = AllTodoRecyclerAdapter(option,mContext)
-            binding.rvAlltodolist.adapter = allTodoRecyclerAdapter
-            binding.rvAlltodolist.layoutManager = LinearLayoutManager(context)
-            allTodoRecyclerAdapter.datas = ArrayList()
-            allTodoRecyclerAdapter.notifyDataSetChanged()
-//            binding.tvPromiseallCount.text = "0"
-        }
+    fun returnsize(): Int = allTodoRecyclerAdapter.itemCount
 
-    }
-
-    suspend fun showOncall() : ArrayList<Oncall>{
-        return suspendCoroutine {
-            continuation ->
-            val responseOncall = getRetrofit().create(RetrofitInterface::class.java) // 이걸 따로 빼내는 방법....
-            val refreshToken = getRefreshToken(requireContext())
-            responseOncall.homeoncallshow(
-                "Bearer $refreshToken",
-            ).enqueue(object :
-                Callback<HomeOncall> { // 서버와 비동기적으로 데이터 주고받을 수 있는 방법 enqueue사용
-                override fun onResponse( // 통신에 성공했을 경우
-                    call: Call<HomeOncall>,
-                    response: Response<HomeOncall>
-                ) {
-                    if (response.isSuccessful) {
-                        val resp = response.body()// 성공했을 경우 response body불러오기
-                        Log.d("SIGNUP/SUCCESS", resp.toString())
-                        Log.d("성공!","success")
-                        continuation.resume(resp!!.result.plans)
-                    }
-                    else{
-                        continuation.resumeWithException(Exception("Response not successful"))
-                    }
-                }
-
-                override fun onFailure(call: Call<HomeOncall>, t: Throwable) { // 통신에 실패했을 경우
-                    Log.d("SIGNUP/FAILURE", t.message.toString()) // 실패한 이유 메세지 출력
-                    continuation.resumeWithException(t)
-                }
-
-            })
-        }
-
-    }
-
-    suspend fun showGroupOncall() : ArrayList<Oncall>{
-        return suspendCoroutine {
-                continuation ->
-            val responseOncall = getRetrofit().create(RetrofitInterface::class.java) // 이걸 따로 빼내는 방법....
-            responseOncall.ShowGroupOncall(
-                groupId,
-            ).enqueue(object :
-                Callback<HomeOncall> { // 서버와 비동기적으로 데이터 주고받을 수 있는 방법 enqueue사용
-                override fun onResponse( // 통신에 성공했을 경우
-                    call: Call<HomeOncall>,
-                    response: Response<HomeOncall>
-                ) {
-                    if (response.isSuccessful) {
-                        val resp = response.body()// 성공했을 경우 response body불러오기
-                        Log.d("SIGNUP/SUCCESS", resp.toString())
-                        Log.d("성공!","success")
-                        continuation.resume(resp!!.result.plans)
-                    }
-                    else{
-                        continuation.resumeWithException(Exception("Response not successful"))
-                    }
-                }
-
-                override fun onFailure(call: Call<HomeOncall>, t: Throwable) { // 통신에 실패했을 경우
-                    Log.d("SIGNUP/FAILURE", t.message.toString()) // 실패한 이유 메세지 출력
-                    continuation.resumeWithException(t)
-                }
-
-            })
-        }
-    }
-
-    fun returnsize() : Int{
-        return allTodoRecyclerAdapter.datas.size
-    }
 }
