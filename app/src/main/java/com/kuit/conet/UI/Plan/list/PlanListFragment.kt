8c10e90e78bca056bf4d3d05cdbf21ea5b3adb05
parkjiwon <ji11won@naver.com>
2024-03-09 03:58:34 +0900

    Refactor : PlanListFragment 리펙토링
    - fragment_plan_list : 레이아웃 수정
    - PlanListFragment : 불필요한 코드 수정, 서버 통신부분 수정, 리펙토링

--- app/src/main/java/com/kuit/conet/UI/Plan/list/PlanListFragment.kt
+++ app/src/main/java/com/kuit/conet/UI/Plan/list/PlanListFragment.kt
@@ -6,15 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-//import com.kuit.conet.Network.ResponseSidePlan
-import com.kuit.conet.Network.RetrofitInterface
-//import com.kuit.conet.Network.SidePlanInfo
-import com.kuit.conet.Network.getRetrofit
-import com.kuit.conet.R
-//import com.kuit.conet.UI.Home.RecyclerView.ConfirmRecyclerAdapter
-import com.kuit.conet.databinding.FragmentGroupBinding
+import com.kuit.conet.Network.RetrofitClient
+import com.kuit.conet.UI.Home.RecyclerView.ConfirmRecyclerAdapter
+import com.kuit.conet.Utils.LIFECYCLE
+import com.kuit.conet.Utils.NETWORK
+import com.kuit.conet.Utils.getRefreshToken
+import com.kuit.conet.data.dto.response.plan.ResponseGetSidebarPlan
 import com.kuit.conet.databinding.FragmentPlanListBinding
+import com.kuit.conet.domain.entity.plan.DecidedPlan
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -31,9 +30,9 @@ class PlanListFragment : Fragment() {
     private var _binding: FragmentPlanListBinding? = null
     private val binding: FragmentPlanListBinding
         get() = requireNotNull(_binding) { "PlanListFragment's binding is null" }
-    private val option: Int by lazy { requireNotNull(arguments?.getInt(PlanVPAdapter.BUNDLE_OPTION)) { "PlanListFragment's option is null" } }
-    private val groupId: Int by lazy { requireNotNull(arguments?.getInt(PlanVPAdapter.BUNDLE_GROUP_ID)) { "PlanListFragment's groupId is null" } }
 
+    private val option: Int by lazy { requireNotNull(arguments?.getInt(PlanVPAdapter.BUNDLE_OPTION)) { "PlanListFragment's option is null" } }      // 0,else : 다가오는 약속, 1 : 지난 약속
+    private val groupId: Int by lazy { requireNotNull(arguments?.getInt(PlanVPAdapter.BUNDLE_GROUP_ID)) { "PlanListFragment's groupId is null" } }
     private val initDeferred = CompletableDeferred<Unit>()
 
     override fun onCreateView(
@@ -41,97 +40,96 @@ class PlanListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPlanListBinding.inflate(inflater, container, false)
+        Log.d(LIFECYCLE, "PlanListFragment - onCreateView() called")
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d(LIFECYCLE, "PlanListFragment - onViewCreated() called")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d(LIFECYCLE, "PlanListFragment - onResume() called")
+
         val coroutineScope = CoroutineScope(Dispatchers.Main)
-//        coroutineScope.launch {
-//            val plans = if (option == 1) {
-//                showSideConfirmplaninfo()
-//            } else {
-//                showSideLastPlan()
-//            }
-//
-//            initConfirmRecycler(plans, option)
-//            initDeferred.complete(Unit)
-//        }
+        coroutineScope.launch {
+            val plans = if (option == 1) {      // 지난 약속
+                showSideConfirmplaninfo()
+            } else {                            // 다가오는 약속
+                showSideLastPlan()
+            }
+
+            binding.rvConfirmlist.adapter = ConfirmRecyclerAdapter(requireContext(), option, plans)
+            initDeferred.complete(Unit)
+        }
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+        Log.d(LIFECYCLE, "PlanListFragment - onDestroyView() called")
     }
 
-//    private fun initConfirmRecycler(item: ArrayList<SidePlanInfo>, option: Int) {
-//        val confirmRecyclerAdapter = ConfirmRecyclerAdapter(requireContext(), option)
-//        binding.rvConfirmlist.adapter = confirmRecyclerAdapter
-//        binding.rvConfirmlist.layoutManager = LinearLayoutManager(context)
-//        confirmRecyclerAdapter.datas = item
-//        confirmRecyclerAdapter.notifyDataSetChanged()
-//    }
-//
-//    private suspend fun showSideConfirmplaninfo(): ArrayList<SidePlanInfo> { // 사이드바 확정된 약속 조회
-//        return suspendCoroutine { continuation2 ->
-//            val responsePlan =
-//                getRetrofit().create(RetrofitInterface::class.java) // 이걸 따로 빼내는 방법....
-//            responsePlan.ShowSideBarConfirm(groupId).enqueue(object :
-//                Callback<ResponseSidePlan> { // 서버와 비동기적으로 데이터 주고받을 수 있는 방법 enqueue사용
-//                override fun onResponse( // 통신에 성공했을 경우
-//                    call: Call<ResponseSidePlan>,
-//                    response: Response<ResponseSidePlan>
-//                ) {
-//                    if (response.isSuccessful) {
-//                        val resp = response.body()// 성공했을 경우 response body불러오기
-//                        Log.d("SIGNUP/SUCCESS", resp.toString())
-//                        Log.d("성공!", "success")
-//                        continuation2.resume(resp!!.result)
-//                    } else {
-//                        continuation2.resumeWithException(Exception("Response not successful"))
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<ResponseSidePlan>, t: Throwable) { // 통신에 실패했을 경우
-//                    Log.d("SIGNUP/FAILURE", t.message.toString()) // 실패한 이유 메세지 출력
-//                    continuation2.resumeWithException(t)
-//                }
-//            })
-//        }
-//
-//    }
-//
-//    private suspend fun showSideLastPlan(): ArrayList<SidePlanInfo> { // 지난 약속 조회
-//        return suspendCoroutine { continuation2 ->
-//            val responsePlan =
-//                getRetrofit().create(RetrofitInterface::class.java) // 이걸 따로 빼내는 방법....
-//            responsePlan.ShowLastPlan(
-//                groupId,
-//            ).enqueue(object :
-//                Callback<ResponseSidePlan> { // 서버와 비동기적으로 데이터 주고받을 수 있는 방법 enqueue사용
-//                override fun onResponse( // 통신에 성공했을 경우
-//                    call: Call<ResponseSidePlan>,
-//                    response: Response<ResponseSidePlan>
-//                ) {
-//                    if (response.isSuccessful) {
-//                        val resp = response.body()// 성공했을 경우 response body불러오기
-//                        Log.d("SIGNUP/SUCCESS", resp.toString())
-//                        Log.d("성공!", "success")
-//                        continuation2.resume(resp!!.result)
-//                    } else {
-//                        continuation2.resumeWithException(Exception("Response not successful"))
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<ResponseSidePlan>, t: Throwable) { // 통신에 실패했을 경우
-//                    Log.d("SIGNUP/FAILURE", t.message.toString()) // 실패한 이유 메세지 출력
-//                    continuation2.resumeWithException(t)
-//                }
-//            })
-//        }
-//    }
+    private suspend fun showSideConfirmplaninfo(): List<DecidedPlan> { // 지난 약속
+        return suspendCoroutine { continuation2 ->
+            RetrofitClient.planInstance.getSidebarPlan(
+                authorization = "Bearer ${getRefreshToken(requireContext())}",
+                teamId = groupId.toLong(),
+                period = "past"
+            ).enqueue(object : Callback<ResponseGetSidebarPlan> {
+                override fun onResponse(
+                    call: Call<ResponseGetSidebarPlan>,
+                    response: Response<ResponseGetSidebarPlan>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.d(NETWORK, "PlanListFragment - getSidebarPlan() 실행 결과 - 성공")
+                        val resp =
+                            requireNotNull(response.body()) { "PlanListFragment - getSidebarPlan() 실행 결과 불러오기 실패" }
+                        continuation2.resume(resp.result.plans.map { it.asDecidedPlan() })
+                    } else {
+                        Log.d(NETWORK, "PlanListFragment - getSidebarPlan() 실행 결과 - 안좋음")
+                        continuation2.resumeWithException(Exception("Response not successful"))
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseGetSidebarPlan>, t: Throwable) {
+                    Log.d(NETWORK, "PlanListFragment - getSidebarPlan() 실행 결과 - 실패\nbecause : $t")
+                    continuation2.resumeWithException(t)
+                }
+            })
+        }
+
+    }
+
+    private suspend fun showSideLastPlan(): List<DecidedPlan> { // 다가오는 약속
+        return suspendCoroutine { continuation2 ->
+            RetrofitClient.planInstance.getSidebarPlan(
+                authorization = "Bearer ${getRefreshToken(requireContext())}",
+                teamId = groupId.toLong(),
+                period = "oncoming"
+            ).enqueue(object : Callback<ResponseGetSidebarPlan> {
+                override fun onResponse(
+                    call: Call<ResponseGetSidebarPlan>,
+                    response: Response<ResponseGetSidebarPlan>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.d(NETWORK, "PlanListFragment - getSidebarPlan() 실행 결과 - 성공")
+                        val resp =
+                            requireNotNull(response.body()) { "PlanListFragment - getSidebarPlan() 실행 결과 불러오기 실패" }
+                        continuation2.resume(resp.result.plans.map { it.asDecidedPlan() })
+                    } else {
+                        Log.d(NETWORK, "PlanListFragment - getSidebarPlan() 실행 결과 - 안좋음")
+                        continuation2.resumeWithException(Exception("Response not successful"))
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseGetSidebarPlan>, t: Throwable) {
+                    Log.d(NETWORK, "PlanListFragment - getSidebarPlan() 실행 결과 - 실패\nbecause : $t")
+                    continuation2.resumeWithException(t)
+                }
+            })
+        }
+    }
 }
