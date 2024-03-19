18ee52990482f7d3b38d660decc0426a5c5ad6b5
parkjiwon <ji11won@naver.com>
2024-03-19 18:55:16 +0900

    Feat : AuthAPI/login 의 Request, Response 생성

---
+++ app/src/main/java/com/kuit/conet/data/dto/response/auth/ResponseLogin.kt
@@ -0,0 +1,22 @@
+package com.kuit.conet.data.dto.response.auth
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseLogin(
+    @SerialName("result")
+    val result: ResultLogin,
+)
+
+@Serializable
+data class ResultLogin(
+    @SerialName("email")
+    val email: String,
+    @SerialName("accessToken")
+    val accessToken: String,
+    @SerialName("refreshToken")
+    val refreshToken: String,
+    @SerialName("isRegistered")
+    val isRegistered: Boolean,
+)
