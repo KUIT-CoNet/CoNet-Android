4ce349c3a9690a06974697963aa136484b6c311d
parkjiwon <ji11won@naver.com>
2024-04-01 18:03:39 +0900

    Feat : Member 인자 추가 & 직렬화
    - inPlan 인자 추가 : 특정 Plan의 참여자로 들어가 있는지를 나타냄
    - Serializable : 직렬화 인터페이스 추가, intent, bundle로 전달시 필요

--- app/src/main/java/com/kuit/conet/domain/entity/member/Member.kt
+++ app/src/main/java/com/kuit/conet/domain/entity/member/Member.kt
@@ -1,18 +1,10 @@
 package com.kuit.conet.domain.entity.member
 
-import com.kuit.conet.Network.Members
+import java.io.Serializable
 
 data class Member(
     val id: Long,
     val name: String,
     val imageUrl: String,
-) {
-    //기존 코드를 위해 임시적으로 넣은 코드
-    fun asMembers(): Members {
-        return Members(
-            id = id.toInt(),
-            name = name,
-            image = imageUrl,
-        )
-    }
-}
+    var inPlan: Boolean = false,
+) : Serializable
