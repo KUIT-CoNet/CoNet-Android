c8143bb4ab89add791285b8424e764fcf7598cef
parkjiwon <ji11won@naver.com>
2024-04-13 15:51:19 +0900

    Feat : MemberAPI 사용자 조회, 이름 수정, 이미지 수정 API 활성화
    - 사용자 조회  : getUserInfo
    - 사용자 이름 수정 :  editUserName
    - 사용자 이미지 수정 : editUserImg

--- app/src/main/java/com/kuit/conet/data/api/MemberAPI.kt
+++ app/src/main/java/com/kuit/conet/data/api/MemberAPI.kt
@@ -1,11 +1,11 @@
 package com.kuit.conet.data.api
 
-import com.kuit.conet.Network.EditUserImage
-import com.kuit.conet.Network.EditUserName
-import com.kuit.conet.Network.ResponseGetGroup
-import com.kuit.conet.Network.ShowUser
+import com.kuit.conet.data.dto.request.member.RequestEditUserName
 import com.kuit.conet.data.dto.request.member.RequestPostBookmark
+import com.kuit.conet.data.dto.response.member.ResponseEditUserImg
+import com.kuit.conet.data.dto.response.member.ResponseEditUserName
 import com.kuit.conet.data.dto.response.member.ResponseGetBookmarkGroups
+import com.kuit.conet.data.dto.response.member.ResponseGetUserInfo
 import com.kuit.conet.data.dto.response.member.ResponsePostBookmark
 import okhttp3.MultipartBody
 import retrofit2.Call
@@ -17,28 +17,28 @@ import retrofit2.http.POST
 import retrofit2.http.Part
 
 interface MemberAPI {
-    /*@GET("member") // :: 사용자 조회
-    fun getUser(
+    @GET("member") // :: 사용자 조회
+    fun getUserInfo(
         @Header("Authorization")
         authorization: String,
-    ): Call<ShowUser>
+    ): Call<ResponseGetUserInfo>
 
     @POST("member/name") // :: 사용자 이름 수정
-    fun editName(
+    fun editUserName(
         @Header("Authorization")
         authorization: String,
         @Body
-        name: String,
-    ): Call<EditUserName>
+        request: RequestEditUserName,
+    ): Call<ResponseEditUserName>
 
     @Multipart
     @POST("member/image") // ::  사용자 이미지 수정
-    fun editImage(
+    fun editUserImg(
         @Header("Authorization")
         authorization: String,
         @Part
         image: MultipartBody.Part,
-    ): Call<EditUserImage>*/
+    ): Call<ResponseEditUserImg>
 
     @GET("member/bookmark") // :: 북마크 된 모임 조회
     fun getBookmarkGroups(
