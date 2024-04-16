ab06dbb441d122ee0d7f27071a01b77a75ad1d94
parkjiwon <ji11won@naver.com>
2024-04-16 16:35:54 +0900

    Feat : NameChangeActivity 반환값 추가
    - 이름 변경 후 통신 객체 변경
    - 통신 성공시 변경된 이름 값 이전 화면으로 전달
    - 코드 포멧팅 & 리펙토토링

--- app/src/main/java/com/kuit/conet/UI/User/NameChangeActivity.kt
+++ app/src/main/java/com/kuit/conet/UI/User/NameChangeActivity.kt
@@ -1,89 +1,78 @@
 package com.kuit.conet.UI.User
 
+import android.app.Activity
 import android.content.Intent
-import android.net.Network
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import com.kuit.conet.*
 import com.kuit.conet.Network.*
+import com.kuit.conet.Utils.LIFECYCLE
 import com.kuit.conet.Utils.NETWORK
 import com.kuit.conet.Utils.getRefreshToken
-import com.kuit.conet.Utils.getUsername
 import com.kuit.conet.Utils.saveUsername
+import com.kuit.conet.data.dto.request.member.RequestEditUserName
+import com.kuit.conet.data.dto.response.member.ResponseEditUserName
 import com.kuit.conet.databinding.ActivityNameChangeBinding
 import retrofit2.Call
 import retrofit2.Response
 import java.util.regex.Pattern
 
 class NameChangeActivity : AppCompatActivity() {
-    lateinit var binding : ActivityNameChangeBinding
-    val pattern = Pattern.compile("[!@#$%^&*(),. ?\":{}|<>]") // 특수 문자 체크
-    var edit = true
+
+    private lateinit var binding: ActivityNameChangeBinding
+    private val pattern = Pattern.compile("[!@#$%^&*(),. ?\":{}|<>]") // 특수 문자 체크
+    private var edit = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityNameChangeBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        Log.d(LIFECYCLE, "NameChangeActivity: onCreate called")
 
-        binding.etName.setText(intent.getStringExtra("name"))
+        binding.etName.setText(intent.getStringExtra(InfoActivity.INTENT_TAG_NAME))
+
         binding.ivNameBackBtn.setOnClickListener {
             finish()
         }
 
-        // 이름 검사(실시간으로)
-        binding.etName.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                Log.d("texting", "입력전")
+        binding.etName.doAfterTextChanged {
+            val matcher = pattern.matcher(it.toString())
+            //Log.d("texting", "${matcher.find()}")
+            //Log.d("text length","${p0!!.length}")
+            if (binding.ivNameTextCancel.visibility == View.GONE && it!!.length > 0) {
+                binding.ivNameTextCancel.visibility = View.VISIBLE
             }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                Log.d("texting", "입력중")
+            if (matcher.find() || it!!.length >= 20 || it!!.length <= 0) {
+                Log.d("texting", "오류")
+                binding.ivNameInfo1.setImageResource(R.drawable.ic_error_red)
+                binding.tvNameInfo1.setTextColor(getColor(R.color.error))
+                binding.ivNameTextCancel.visibility = View.GONE
+                binding.ivNameErrorEmpty.visibility = View.VISIBLE
+                binding.cvNameDoneBtn.setBackgroundResource(R.drawable.button_design_gray)
+                binding.llNameUnderline.setBackgroundResource(R.color.error)
+                edit = false
+            } else if (it.length > 0) {
+                binding.ivNameInfo1.setImageResource(R.drawable.ic_error_purple)
+                binding.tvNameInfo1.setTextColor(getColor(R.color.gray800))
+                binding.ivNameTextCancel.visibility = View.VISIBLE
+                binding.ivNameErrorEmpty.visibility = View.GONE
+                binding.cvNameDoneBtn.setBackgroundResource(R.drawable.button_design_purple)
+                binding.llNameUnderline.setBackgroundResource(R.color.gray200)
+                edit = true
+            } else {
+                binding.ivNameInfo1.setImageResource(R.drawable.ic_error_purple)
+                binding.tvNameInfo1.setTextColor(getColor(R.color.gray800))
+                binding.ivNameTextCancel.visibility = View.GONE
+                binding.ivNameErrorEmpty.visibility = View.GONE
+                binding.cvNameDoneBtn.setBackgroundResource(R.drawable.button_design_gray)
+                binding.llNameUnderline.setBackgroundResource(R.color.gray200)
+                edit = false
             }
-
-            override fun afterTextChanged(p0: Editable?) {
-                Log.d("texting", "입력끝")
-
-                val matcher = pattern.matcher(p0.toString())
-                //Log.d("texting", "${matcher.find()}")
-                //Log.d("text length","${p0!!.length}")
-                if (binding.ivNameTextCancel.visibility == View.GONE && p0!!.length > 0) {
-                    binding.ivNameTextCancel.visibility = View.VISIBLE
-                }
-
-                if (matcher.find() || p0!!.length >= 20 || p0!!.length <= 0) {
-                    Log.d("texting", "오류")
-                    binding.ivNameInfo1.setImageResource(R.drawable.ic_error_red)
-                    binding.tvNameInfo1.setTextColor(getColor(R.color.error))
-                    binding.ivNameTextCancel.visibility = View.GONE
-                    binding.ivNameErrorEmpty.visibility = View.VISIBLE
-                    binding.cvNameDoneBtn.setBackgroundResource(R.drawable.button_design_gray)
-                    binding.llNameUnderline.setBackgroundResource(R.color.error)
-                    edit = false
-                } else if (p0.length > 0) {
-                    binding.ivNameInfo1.setImageResource(R.drawable.ic_error_purple)
-                    binding.tvNameInfo1.setTextColor(getColor(R.color.gray800))
-                    binding.ivNameTextCancel.visibility = View.VISIBLE
-                    binding.ivNameErrorEmpty.visibility = View.GONE
-                    binding.cvNameDoneBtn.setBackgroundResource(R.drawable.button_design_purple)
-                    binding.llNameUnderline.setBackgroundResource(R.color.gray200)
-                    edit = true
-                } else {
-                    binding.ivNameInfo1.setImageResource(R.drawable.ic_error_purple)
-                    binding.tvNameInfo1.setTextColor(getColor(R.color.gray800))
-                    binding.ivNameTextCancel.visibility = View.GONE
-                    binding.ivNameErrorEmpty.visibility = View.GONE
-                    binding.cvNameDoneBtn.setBackgroundResource(R.drawable.button_design_gray)
-                    binding.llNameUnderline.setBackgroundResource(R.color.gray200)
-                    edit = false
-                }
-
-            }
-
-        })
+        }
 
         binding.ivNameTextCancel.setOnClickListener {
             binding.etName.text = null
@@ -92,35 +81,49 @@ class NameChangeActivity : AppCompatActivity() {
         binding.cvNameDoneBtn.setOnClickListener { // 완료 시 이름 저장
             if (edit) {
                 saveUsername(this, binding.etName.text.toString())
-                editUserName(binding.etName.text.toString()) //서버로 정보 보내기
-//                val intent = Intent(this, InfoActivity::class.java)
-//                startActivity(intent)
-                finish()
+                editUserName(binding.etName.text.toString())
             }
         }
     }
 
-    private fun editUserName(name : String){
+    private fun editUserName(name: String) {
         Log.d(NETWORK, "editUserName: 실행")
-        val refreshToken = getRefreshToken(this)
-        val editName = getRetrofit().create(RetrofitInterface::class.java)
-        editName.editName(
-            "Bearer $refreshToken",
-            name
-        ).enqueue(object : retrofit2.Callback<EditUserName>{
-            override fun onResponse(call: Call<EditUserName>, response: Response<EditUserName>) {
-                if(response.isSuccessful){
-                    val resp = response.body()
-                    Log.d(NETWORK, resp.toString())
+
+        RetrofitClient.memberInstance.editUserName(
+            authorization = "Bearer ${getRefreshToken(this)}",
+            request = RequestEditUserName(
+                name = name,
+            ),
+        ).enqueue(object : retrofit2.Callback<ResponseEditUserName> {
+            override fun onResponse(
+                call: Call<ResponseEditUserName>,
+                response: Response<ResponseEditUserName>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d(NETWORK, "NameChangeActivity - editUsername 실행결과 - 성공")
+
+                    val returnIntent = Intent()
+                    returnIntent.putExtra(INTENT_TAG_NAME, name)
+                    setResult(Activity.RESULT_OK, returnIntent)
+
+                    finish()
+
                 } else {
-                    Log.d(NETWORK, "onResponse: 실패함")
+                    Log.d(NETWORK, "NameChangeActivity - editUsername 실행결과 - 안좋음")
                 }
             }
 
-            override fun onFailure(call: Call<EditUserName>, t: Throwable) {
-                Log.d(NETWORK, t.message.toString()) // 실패한 이유 메세지 출력
+            override fun onFailure(call: Call<ResponseEditUserName>, t: Throwable) {
+                Log.d(
+                    NETWORK,
+                    "NameChangeActivity - editUsername 실행결과 - 실패\nbecause : ${t.message}"
+                )
             }
 
         })
+    }
+
+    companion object {
+        const val INTENT_TAG_NAME = "name"
     }
 }
