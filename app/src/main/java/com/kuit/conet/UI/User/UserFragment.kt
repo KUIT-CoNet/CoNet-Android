bfc5c407111da707d58c826bb0af9d496ecc7db5
parkjiwon <ji11won@naver.com>
2024-04-16 16:36:21 +0900

    Feat : 사용자 정보 InfoActivity에 전달

--- app/src/main/java/com/kuit/conet/UI/User/UserFragment.kt
+++ app/src/main/java/com/kuit/conet/UI/User/UserFragment.kt
@@ -9,12 +9,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.kuit.conet.*
-import com.kuit.conet.Network.*
+import com.kuit.conet.Network.RetrofitClient
+import com.kuit.conet.R
 import com.kuit.conet.Utils.LIFECYCLE
-import com.kuit.conet.Utils.getUserImg
-import com.kuit.conet.Utils.getUsername
+import com.kuit.conet.Utils.NETWORK
+import com.kuit.conet.Utils.getRefreshToken
+import com.kuit.conet.data.dto.response.member.ResponseGetUserInfo
 import com.kuit.conet.databinding.FragmentUserBinding
+import com.kuit.conet.domain.entity.user.User
 
 
 class UserFragment : Fragment() {
@@ -22,7 +24,7 @@ class UserFragment : Fragment() {
     private var _binding: FragmentUserBinding? = null
     private val binding
         get() = requireNotNull(_binding) { "UserFragment's binding is null" }
-    private lateinit var userData: ShowUserInfo
+    private lateinit var userInfo: User
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,7 +42,7 @@ class UserFragment : Fragment() {
 
         binding.cvUserInfo.setOnClickListener {
             val intent = Intent(requireContext(), InfoActivity::class.java)
-            intent.putExtra("imageURL", getUserImg(requireContext()))
+            intent.putExtra(INTENT_TAG_USER, userInfo)
             startActivity(intent)
         }
 
@@ -68,57 +70,47 @@ class UserFragment : Fragment() {
                 .replace(R.id.cl_user, logoutDialog)
                 .commit()
         }
-
-        binding.tvUserName.text = getUsername(requireContext())
-
-        Glide.with(requireContext())
-            .load(getUserImg(requireContext())) // 불러올 이미지 url
-            .placeholder(R.drawable.profile_purple) // 이미지 로딩 시작하기 전 표시할 이미지
-            .error(R.drawable.profile_purple) // 로딩 에러 발생 시 표시할 이미지
-            .fallback(R.drawable.profile_purple) // 로드할 url 이 비어있을(null 등) 경우 표시할 이미지
-            .circleCrop() // 동그랗게 자르기 동그란 맘속에 피어난 How is the life...
-            .into(binding.ivUserProfile) // 이미지를 넣을 뷰
     }
+
 
     override fun onStart() {
         super.onStart()
         Log.d(LIFECYCLE, "UserFragment: onStart called")
 
+        RetrofitClient.memberInstance.getUserInfo(
+            authorization = "Bearer ${getRefreshToken(requireActivity())}"
+        ).enqueue(object : retrofit2.Callback<ResponseGetUserInfo> {
+            override fun onResponse(
+                call: retrofit2.Call<ResponseGetUserInfo>,
+                response: retrofit2.Response<ResponseGetUserInfo>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d(NETWORK, "UserFragment - getUserInfo 호출 결과 - 성공")
+
+                    val resp =
+                        requireNotNull(response.body()) { "UserFragment - getUserInfo 호출 결과 불러오기 실패" }
+                    userInfo = resp.result.asUser()
+
+                    binding.tvUserName.text = userInfo.name
+                    Glide.with(requireContext())
+                        .load(userInfo.imgUrl)
+                        .placeholder(R.drawable.profile_purple)
+                        .error(R.drawable.profile_purple)
+                        .fallback(R.drawable.profile_purple)
+                        .circleCrop()
+                        .into(binding.ivUserProfile)
+                } else {
+                    Log.d(NETWORK, "UserFragment - getUserInfo 호출 결과 - 안좋음")
+                }
+            }
+
+            override fun onFailure(call: retrofit2.Call<ResponseGetUserInfo>, t: Throwable) {
+                Log.d(NETWORK, "UserFragment - getUserInfo 호출 결과 - 실패\nbecause : ${t.message}")
+            }
+        })
     }
 
-//    private fun callUserInfo() {
-//        var responseUser = getRetrofit().create(RetrofitInterface::class.java)
-//        val refreshToken = getRefreshToken(requireContext())
-//        responseUser.showuser(
-//            "Bearer $refreshToken"
-//        ).enqueue(object :
-//            retrofit2.Callback<ShowUser>{
-//            override fun onResponse(call: Call<ShowUser>, response: Response<ShowUser>) {
-//                if (response.isSuccessful) {
-//                    var resp = response.body()// 성공했을 경우 response body불러오기
-//                    Log.d(NETWORK, resp.toString())
-//                    var userData  = resp!!.result
-//                    binding.tvUserName.text = userData.name
-//                    var imageUri = userData.memberImgUrl.toUri()
-//                    saveUserImgUrl(requireContext(), imageUri.toString())
-//                    Glide.with(requireContext())
-//                        .load(imageUri) // 로컬 파일로 변환한 이미지를 로드
-//                        .placeholder(R.drawable.profile_purple)
-//                        .error(R.drawable.profile_purple)
-//                        .fallback(R.drawable.profile_purple)
-//                        .circleCrop()
-//                        .into(binding.ivUserProfile)
-//                } else {
-////                    continuation.resumeWithException(Exception("Response not successful"))
-//                    Log.d(NETWORK, "[callUserInfo in User] onResponse: Response not successful")
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<ShowUser>, t: Throwable) {
-//                Log.d(NETWORK, t.message.toString()) // 실패한 이유 메세지 출력
-////                continuation.resumeWithException(t)
-//            }
-//
-//        })
-//    }
+    companion object {
+        const val INTENT_TAG_USER = "userInfo"
+    }
 }
