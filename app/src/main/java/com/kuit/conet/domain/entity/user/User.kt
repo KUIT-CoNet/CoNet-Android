977545ece825cb6be56072b967fb37f4621aa04d
parkjiwon <ji11won@naver.com>
2024-04-16 16:32:49 +0900

    Feat : 직렬화 추가, var로 변경
     - 데이터 전달을 위해서 Serializable 추가
    - name, imgUrl 변경에 용이하게 val -> var 변경

--- app/src/main/java/com/kuit/conet/domain/entity/user/User.kt
+++ app/src/main/java/com/kuit/conet/domain/entity/user/User.kt
@@ -1,12 +1,14 @@
 package com.kuit.conet.domain.entity.user
 
+import java.io.Serializable
+
 data class User(
-    val name: String,
-    val imgUrl: String,
+    var name: String,
+    var imgUrl: String,
     val emails: List<Email>,
-) {
+) : Serializable {
     data class Email(
         val email: String,
         val platform: String,       // TODO 나중에 enum, sealed class로 변경
-    )
+    ) : Serializable
 }
