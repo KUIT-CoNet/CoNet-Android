2967fdecb349fa65c9e74e2f0b632d9d0557ec91
parkjiwon <ji11won@naver.com>
2024-05-29 04:55:37 +0900

    Feat : SplashActivity DataStore 이전

--- app/src/main/java/com/kuit/conet/UI/SplashActivity.kt
+++ app/src/main/java/com/kuit/conet/UI/SplashActivity.kt
@@ -5,16 +5,21 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.lifecycleScope
 import com.kuit.conet.UI.Login.LoginActivity
 import com.kuit.conet.Network.RetrofitClient
+import com.kuit.conet.UI.application.CoNetApplication
 import com.kuit.conet.Utils.LIFECYCLE
 import com.kuit.conet.Utils.NETWORK
 import com.kuit.conet.databinding.ActivitySplashBinding
 import com.kuit.conet.Utils.getRefreshToken
 import com.kuit.conet.Utils.saveUserRefreshToken
 import com.kuit.conet.data.dto.response.auth.ResponseRenewTokens
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Response
+import java.nio.charset.Charset
 
 class SplashActivity : AppCompatActivity() {
 
@@ -27,12 +32,19 @@ class SplashActivity : AppCompatActivity() {
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        renewTokens(getRefreshToken(this))
+        lifecycleScope.launch {
+            val refreshToken =
+                CoNetApplication.getInstance().getDataStore().bearerRefreshToken.first()
+
+            renewTokens(refreshToken)
+        }
+
+//        renewTokens(getRefreshToken(this))
     }
 
     private fun renewTokens(refreshToken: String) {
         RetrofitClient.authInstance.renewTokens(
-            refreshToken = "Bearer $refreshToken"
+            refreshToken = refreshToken
         ).enqueue(object : retrofit2.Callback<ResponseRenewTokens> {
             override fun onResponse(
                 call: Call<ResponseRenewTokens>,
@@ -43,11 +55,21 @@ class SplashActivity : AppCompatActivity() {
                     val resp =
                         requireNotNull(response.body()) { "SplashActivity - getAccess() 결과 불러오기 실패" }
 
-                    saveUserRefreshToken(applicationContext, resp.result.refreshToken)
+                    lifecycleScope.launch {
+                        CoNetApplication.getInstance().getDataStore().also {
+                            it.setAccesToken(resp.result.accessToken)
+                            it.setRefreshToken(resp.result.refreshToken)
+                        }
+
+                        val mIntent = Intent(applicationContext, ConetMainActivity::class.java)
+                        startActivity(mIntent)
+                        finish()
+                    }
+                    /*saveUserRefreshToken(applicationContext, resp.result.refreshToken)
 
                     val mIntent = Intent(applicationContext, ConetMainActivity::class.java)
                     startActivity(mIntent)
-                    finish()
+                    finish()*/
                 } else {
                     Log.d(NETWORK, "SplashActivity - getAccess() 실행결과 안좋음\nresponse : $response")
                     val mIntent = Intent(applicationContext, LoginActivity::class.java)
