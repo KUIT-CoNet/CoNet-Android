9dfc5712f28020c3154f47b26a0da7b4b31c37cc
parkjiwon <ji11won@naver.com>
2024-07-16 04:05:45 +0900

    Feat : 날짜를 표시하는 기준 변경
    - CalendarDay : MaterialCalendarView 라이브러리에서 사용하기 때문에 유지
    - Calendar : 삭제
    - LocalDate : 날짜를 표시하는 기준으로 변경

--- app/src/main/java/com/kuit/conet/UI/Home/HomeFragment.kt
+++ app/src/main/java/com/kuit/conet/UI/Home/HomeFragment.kt
@@ -10,12 +10,15 @@ import com.kuit.conet.Utils.LIFECYCLE
 import com.kuit.conet.Utils.OncallList
 import com.kuit.conet.Utils.TAG
 import com.kuit.conet.Utils.Todolist
+import com.kuit.conet.Utils.calendar.compareWithLocalDate
+import com.kuit.conet.Utils.calendar.toString
 import com.kuit.conet.databinding.FragmentHomeBinding
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.util.*
 
 class HomeFragment : Fragment() {
@@ -38,18 +41,16 @@ class HomeFragment : Fragment() {
         Log.d(LIFECYCLE, "HomeFragment - onViewCreated() called")
 
         val calendarFragment = CalendarFragment()
+//        CalendarFragment의 달력 화면에서 일/월을 변경하였을 때
         calendarFragment.setOnDateChangedListener { widget, date, selected ->
-            val selectedDateString = dateString(date.date, 1)
-            val today = Date() // 오늘 날짜
-            val todayString = dateString(today, 1)
-            Log.d(TAG, selectedDateString)
+            val today = LocalDate.now()
 
-            if (todayString == selectedDateString) {
+            if (date.compareWithLocalDate(today)) {
                 binding.tvPromiseDate.text = "오늘의 약속"
             } else {
-                binding.tvPromiseDate.text = "$selectedDateString 의 약속"
+                binding.tvPromiseDate.text = "${date.toString(false)} 의 약속"
             }
-            Log.d(TAG, dateString(date.date, 2))
+
             val todolist = Todolist(date, -1)
             parentFragmentManager.beginTransaction()
                 .replace(R.id.fl_todolist, todolist)
@@ -94,14 +95,13 @@ class HomeFragment : Fragment() {
 
     private fun dateString(
         date: Date,
-        option: Int
+        isyear: Boolean,
     ): String { // option이 1이면 m월 d일 형식, option이 2면 yyyy년 m월 d일 형식
-        val dateFormat: SimpleDateFormat = if (option == 1) {
-            SimpleDateFormat("M월 d일", Locale.getDefault())
+        return if (isyear) {
+            SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault()).format(date)
         } else {
-            SimpleDateFormat("yyyy년 M월 d일", Locale.getDefault())
+            SimpleDateFormat("MM월 dd일", Locale.getDefault()).format(date)
         }
-        return dateFormat.format(date)
     }
 
 }
--- app/src/main/java/com/kuit/conet/Utils/calendar/CalendarDayExt.kt
+++ app/src/main/java/com/kuit/conet/Utils/calendar/CalendarDayExt.kt
@@ -1,14 +1,32 @@
 package com.kuit.conet.Utils.calendar
 
 import com.prolificinteractive.materialcalendarview.CalendarDay
+import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.util.Calendar
+import java.util.Locale
 
-fun CalendarDay.asCalendar(): Calendar {
-    val calendar = Calendar.getInstance()
-    calendar.set(year, month, day)
-    return calendar
+fun CalendarDay.toLocalDate(): LocalDate {
+    return LocalDate.of(year, month + 1, day)
 }
 
-fun Calendar.asCalendarDay(): CalendarDay {
-    return CalendarDay.from(get(Calendar.YEAR), get(Calendar.MONTH), get(Calendar.DATE))
+fun CalendarDay.compareWithLocalDate(date: LocalDate): Boolean {
+    /*LocalDate
+    *   - year: 년
+    *   - monthValue: 월(1~12) / month: 월(Month! - enum class)
+    *   - dayOfMonth: 일
+    * CalendarDay
+    *   - year: 년
+    *   - month: 월(0~11)
+    *   - day: 일
+    * */
+    return year == date.year && month + 1 == date.monthValue && day == date.dayOfMonth
+}
+
+fun CalendarDay.toString(isYear: Boolean): String {
+    return if (isYear) {
+        SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault()).format(date)
+    } else {
+        SimpleDateFormat("MM월 dd일", Locale.getDefault()).format(date)
+    }
 }
