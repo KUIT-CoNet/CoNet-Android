22f46206bb8f2c2b4afc4a7e5835877d858196b3
parkjiwon <ji11won@naver.com>
2024-07-20 19:43:14 +0900

    Feat : CalendarDay와 LocalDate를 비교하는 함수 추가
    현재 같은 날인지 비교하는 값만 있지만 이후 년도가 달라질 때 결과를 내기 위해서 새로운 함수 추가

--- app/src/main/java/com/kuit/conet/Utils/calendar/CalendarDayExt.kt
+++ app/src/main/java/com/kuit/conet/Utils/calendar/CalendarDayExt.kt
@@ -23,6 +23,23 @@ fun CalendarDay.compareWithLocalDate(date: LocalDate): Boolean {
     return year == date.year && month + 1 == date.monthValue && day == date.dayOfMonth
 }
 
+// CalendarDay와 LocalDate를 비교하여 결과를 반환하는 함수
+/*fun CalendarDay.compareWithLocalDate(date: LocalDate): RESULT_COMPARE_LOCALDATE {
+    if (year != date.year) {
+        return RESULT_COMPARE_LOCALDATE.DIFFERENT_YEAR
+    } else if (month + 1 != date.monthValue) {
+        return RESULT_COMPARE_LOCALDATE.DIFFERENT_MONTH
+    } else if (day != date.dayOfMonth) {
+        return RESULT_COMPARE_LOCALDATE.DIFFERENT_DAY
+    } else {
+        return RESULT_COMPARE_LOCALDATE.SAME
+    }
+}
+
+enum class RESULT_COMPARE_LOCALDATE {
+    SAME, DIFFERENT_YEAR, DIFFERENT_MONTH, DIFFERENT_DAY
+}*/
+
 fun CalendarDay.toString(isYear: Boolean): String {
     return if (isYear) {
         SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault()).format(date)
